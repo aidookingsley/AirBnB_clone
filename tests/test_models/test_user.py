9b7c6f617606d535202093cbbeeff3ac0a82dc4e
kingsley aidoo <kaidoog36@gmail.com>
2022-09-05 12:29:26 +0000

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
-''' module for user tests '''
-from unittest import TestCase
-import json
-import re
-from uuid import UUID, uuid4
-from datetime import datetime
-from time import sleep
+"""test module for class User"""
 
-from models.base_model import BaseModel
-from models.user import User
+import models
+import datetime
+import unittest
 
 
-class TestUser(TestCase):
-    ''' tests User class '''
-    def test_8(self):
-        ''' task 8 tests '''
-        self.assertTrue(issubclass(User, BaseModel))
-        self.assertEqual(User.email, '')
-        self.assertEqual(User.password, '')
-        self.assertEqual(User.first_name, '')
-        self.assertEqual(User.last_name, '')
+class UserTest(unittest.TestCase):
+    """tests the class User"""
+
+    def test_documentation(self):
+        """tests module and class docstring"""
+        self.assertIsNotNone(models.user.__doc__)
+        self.assertIsNotNone(models.user.User.__doc__)
+
+    def test_class(self):
+        """test instance class"""
+        instance = models.user.User()
+        self.assertIsInstance(instance, models.user.User)
+
+    def test_type(self):
+        """test type of instance atributes"""
+        instance = models.user.User()
+        self.assertIsInstance(instance.id, str)
+        self.assertIsInstance(instance.created_at, datetime.datetime)
+        self.assertIsInstance(instance.updated_at, datetime.datetime)
+        self.assertIsInstance(instance.email, str)
+        self.assertIsInstance(instance.password, str)
+        self.assertIsInstance(instance.first_name, str)
+        self.assertIsInstance(instance.last_name, str)
+
+
+if __name__ == "__main__":
+    unittest.main()
